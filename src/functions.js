939be9469f8ba3ee96af847334f0f6aff7de4388
romankomprs <rkomprs@gmail.com>
2021-10-08 07:51:27 +0200

    bootstrap setup and ready

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+import * as bootstrap from "bootstrap";
 // const { JSDOM } = require( "jsdom" );
 // const { window } = new JSDOM( "" );
 // const $ = require( "jquery" )( window );
@@ -6,6 +7,9 @@
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery; // notice the definition of global variables here
 // require("jquery-ui-dist/jquery-ui.js");
+// require('bootstrap');
+
+
 
 $(function() {
 //   $("#datepicker").datepicker();
@@ -14,6 +18,35 @@ $(function() {
     });
 
 
+    $("button.ajaxButton").click(function(){
+        $.getJSON({url: "https://jsonplaceholder.typicode.com/todos/1", success: function(result){
+          $("#ajaxResponse").html(result);
+          console.log(result);
+        }});
+      });
+
+      $("button.ajaxButton").click(function(){
+
+    $.getJSON( "https://jsonplaceholder.typicode.com/todos/1", {
+        format: "json"
+        // $("#ajaxResponse").html("JSON Data: " + "title: " + json.title + "useID " + json.id );
+        // console.log( "JSON Data: " + "title: " + json.title + "useID " + json.id );
+       })
+       .done(function( data ) {
+           console.log("data: ",data);
+            $.each( data, function( i, item ) {
+                console.log(i + ": ",item);
+                //   $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+                //   if ( i === 3 ) {
+                //     return false;
+                //   }
+            });
+      })
+      .fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+    });
+    });
 
 
 
@@ -26,6 +59,17 @@ $(function() {
 
 // ajax request
 (function() {
+
+
+
+
+
+
+
+
+
+
+
     $('#ajaxForm').bind('submit', function(evetn){
         event.preventDefault();
         var  search = $('#title').val();
